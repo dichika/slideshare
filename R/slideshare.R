b8b96940896e5dc0c6d2989fcf41107c1df0eb31
dichika <ichikawa@hcc-jp.com>
2014-03-07 20:02:16 +0900

    add new method

--- R/slideshare.R
+++ R/slideshare.R
@@ -52,3 +52,28 @@ Slideshare$methods(
     r <- xmlRoot(docxmlparsed)
     res <- data.frame(t(xmlSApply(r, xmlValue)), stringsAsFactors=FALSE)
   })
+
+Slideshare$methods(
+  getSlideshowByTag = function(tag=NULL, limit=10) {
+    require(XML)
+    require(digest)
+    require(RCurl)
+    if(is.null(tag)){
+      stop("tag must be required")
+    }
+    u <- "https://www.slideshare.net/api/2/get_slideshows_by_tag?"
+    tstamp <- as.numeric(as.POSIXlt(Sys.time()))
+    hash <- digest(paste0(sharedsecret, tstamp),algo="sha1", serialize=FALSE)
+    url <- paste0(u,
+                  "api_key=", apikey,
+                  "&ts=", tstamp,
+                  "&hash=", hash,
+                  "&tag=", tag,
+                  "&limit=", limit,
+                  "&detailed=1")
+    docxml <- getURL(url)
+    res <- xmlToDataFrame(docxml,stringsAsFactors=FALSE)
+    res <- subset(res, !is.na(ID))
+    res$text <- NULL
+    invisible(res)
+  })
