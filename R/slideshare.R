08e47f90c89e7d02efa3d0974145857a8b806493
dichika <ichikawa@hcc-jp.com>
2014-03-08 21:12:38 +0900

    add new functions

--- R/slideshare.R
+++ R/slideshare.R
@@ -84,4 +84,91 @@ Slideshare$methods(
     invisible(res)
   })
 
+createSlideshowByTag <- function(tag=NULL, limit=100, apikey=NULL, sharedsecret=NULL, file="output.html"){
+  require(rCharts)  
+  # get data    
+  if(is.null(tag)){
+    stop("tag must be required")
+  }
+  if(!(limit>0)){
+    stop("limit must be greater than 0")
+  }
+  if((is.null(apikey)||is.null(sharedsecret))){
+    stop("APIkey and Sharedsecret must be required")
+  }
+  ss <- Slideshare$new(apikey=apikey,sharedsecret=sharedsecret)
+  res <- ss$getSlideshowByTag(tag,limit=limit)
+  
+  res$EmbedModified <- gsub('width="427"','width="600"',res$Embed)
+  releaseList <- plyr::alply(res, 1, function(x) {
+    list(startDate = format(as.Date(x$Created), "%Y,%m,%d"), 
+         headline = x$Title, 
+         text = x$EmbedModified,
+         asset=list()
+    )
+  })
+  
+  m = Timeline$new()
+  m$main(headline = res[res$Created==min(res$Created),"Title"],
+         type = "default", 
+         text = res[res$Created==min(res$Created),"EmbedModified"],
+         startDate = format(as.Date(min(res$Created)), "%Y,%m,%d"), 
+         asset = list()
+  )
+  names(releaseList) <- NULL
+  m$event(releaseList)
+  tmp <- tempfile()
+  m$save(tmp)
+  
+  x <- paste(readLines(tmp, warn = F), collapse = "\n")
+  x <- gsub(paste0(system.file(package="rCharts"),"/libraries/timeline/js/storyjs-embed.js"), 
+            "http://timelyportfolio.github.io/rCharts_timeline_r/compiled/js/storyjs-embed.js", 
+            x)
+  writeLines(x, con = file)
+}
+
+createSlideshowByUser <- function(user=NULL, limit=100, apikey=NULL, sharedsecret=NULL, file="output.html"){
+  require(rCharts)  
+  # get data    
+  if(is.null(user)){
+    stop("user must be required")
+  }
+  if(!(limit>0)){
+    stop("limit must be greater than 0")
+  }
+  if((is.null(apikey)||is.null(sharedsecret))){
+    stop("APIkey and Sharedsecret must be required")
+  }
+  ss <- Slideshare$new(apikey=apikey,sharedsecret=sharedsecret)
+  res <- ss$getSlideshowByUser(user,limit=limit)
+  
+  res$EmbedModified <- gsub('width="427"','width="600"',res$Embed)
+  releaseList <- plyr::alply(res, 1, function(x) {
+    list(startDate = format(as.Date(x$Created), "%Y,%m,%d"), 
+         headline = x$Title, 
+         text = x$EmbedModified,
+         asset=list()
+    )
+  })
+  
+  m = Timeline$new()
+  m$main(headline = res[res$Created==min(res$Created),"Title"],
+         type = "default", 
+         text = res[res$Created==min(res$Created),"EmbedModified"],
+         startDate = format(as.Date(min(res$Created)), "%Y,%m,%d"), 
+         asset = list()
+  )
+  names(releaseList) <- NULL
+  m$event(releaseList)
+  tmp <- tempfile()
+  m$save(tmp)
+  
+  x <- paste(readLines(tmp, warn = F), collapse = "\n")
+  x <- gsub(paste0(system.file(package="rCharts"),"/libraries/timeline/js/storyjs-embed.js"), 
+            "http://timelyportfolio.github.io/rCharts_timeline_r/compiled/js/storyjs-embed.js", 
+            x)
+  writeLines(x, con = file)
+}
+
+
 # test_file("inst/tests/test-slideshare.R")
