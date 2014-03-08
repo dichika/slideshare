2ff4802247c8f6a5e50e7cdb81b874d9ae49c9f0
dichika <ichikawa@hcc-jp.com>
2014-03-08 13:39:32 +0900

    add a new method

--- R/slideshare.R
+++ R/slideshare.R
@@ -5,28 +5,6 @@ Slideshare <- setRefClass(Class="Slideshare",
                   )
 
 Slideshare$methods(
-  getSlideshowById = function(id=NULL) {
-  require(XML)
-  require(digest)
-  require(RCurl)
-  if(is.null(id)){
-    stop("id must be required")
-  }
-  u <- "https://www.slideshare.net/api/2/get_slideshow?"
-  tstamp <- as.numeric(as.POSIXlt(Sys.time()))
-  hash <- digest(paste0(sharedsecret, tstamp),algo="sha1", serialize=FALSE)
-  url <- paste0(u, 
-                "slideshow_id=", id,
-                "&api_key=", apikey,
-                "&ts=", tstamp,
-                "&hash=", hash)
-  docxml <- getURL(url)
-  docxmlparsed <- xmlInternalTreeParse(docxml)
-  r <- xmlRoot(docxmlparsed)
-  res <- data.frame(t(xmlSApply(r, xmlValue)))
-})
-
-Slideshare$methods(
   getSlideshow = function(url=NULL, id=NULL) {
     require(XML)
     require(digest)
@@ -54,12 +32,15 @@ Slideshare$methods(
   })
 
 Slideshare$methods(
-  getSlideshowByTag = function(tag=NULL, limit=10) {
+  getSlideshowByTag = function(tag=NULL, limit=100) {
     require(XML)
     require(digest)
     require(RCurl)
     if(is.null(tag)){
       stop("tag must be required")
+    }
+    if(!(limit>0)){
+      stop("limit must be greater than 0")
     }
     u <- "https://www.slideshare.net/api/2/get_slideshows_by_tag?"
     tstamp <- as.numeric(as.POSIXlt(Sys.time()))
@@ -77,3 +58,30 @@ Slideshare$methods(
     res$text <- NULL
     invisible(res)
   })
+
+Slideshare$methods(
+  getSlideshowByUser = function(user=NULL, limit=NULL) {
+    require(XML)
+    require(digest)
+    require(RCurl)
+    if(is.null(user)){
+      stop("user must be required")
+    }
+    u <- "https://www.slideshare.net/api/2/get_slideshows_by_user?"
+    tstamp <- as.numeric(as.POSIXlt(Sys.time()))
+    hash <- digest(paste0(sharedsecret, tstamp),algo="sha1", serialize=FALSE)
+    url <- paste0(u,
+                  "api_key=", apikey,
+                  "&ts=", tstamp,
+                  "&hash=", hash,
+                  "&username_for=", user,
+                   ifelse(!(is.null(limit)), paste0("&limit=", limit), ""),
+                  "&detailed=1")
+    docxml <- getURL(url)
+    res <- xmlToDataFrame(docxml,stringsAsFactors=FALSE)
+    res <- subset(res, !is.na(ID))
+    res$text <- NULL
+    invisible(res)
+  })
+
+# test_file("inst/tests/test-slideshare.R")
